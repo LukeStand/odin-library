8333e71d813eb76fbd11f3efc45e8a219ff0cfb6
Luke Stand <92410656+LukeStand@users.noreply.github.com>
2025-07-31 18:54:44 -0700

    general layout complete will do js later

---
+++ main.js
@@ -0,0 +1,10 @@
+const libraryArr = [];
+//random id generator
+crypto.randomUUID()
+//constructor for a book
+function Book(name,id,){
+
+}
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
