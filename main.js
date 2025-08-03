f0c25e313e1a633e84219b25b9d7fd7bbb469972
Luke Stand <92410656+LukeStand@users.noreply.github.com>
2025-08-03 09:53:08 -0700

    finished general layout of html and css will start js logic

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 const libraryArr = [];
 //random id generator
 crypto.randomUUID()
-//constructor for a book
-function Book(name,id,){
 
+
+const dialog = document.querySelector("#myModal");
+const openBtn = document.querySelector("#openModal-btn");
+const closeBtn = document.querySelector("#closeModal-btn");
+const addBookBtn = document.querySelector("#addBookBtn");
+
+openBtn.addEventListener("click",function(){
+    dialog.showModal();
+});
+closeBtn.addEventListener("click", function(){
+    dialog.close();
+});
+
+addBookBtn.addEventListener("click",function(){
+    dialog.close();
+});
+
+
+//constructor for a book
+function Book(author, title, pages, isRead,id){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.id = id;
 }
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+// take params, create a book then store it in the array
+function addBookToLibrary(author,title,pages,isRead,id) 
+{
+    libraryArr.push(newBook);
+    const newBook = new Book(author,title,pages,isRead,id);
+  
 }
